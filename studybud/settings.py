e2f869acc92dbb0a5407338788cd55cb2933e190
AliNiaz990487 <alik990487@gmail.com>
2024-03-08 17:35:26 +0500

    allow to view on mobile

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1e!@xjrmf63*j=du$3n=xrcbrn$4)zpm7mb6uuo#c_0gltlqrd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.239.199"]
 
 
 # Application definition
