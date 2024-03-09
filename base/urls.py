a24d4faccf9c25b8f812f3d98f6babef024fdf91
AliNiaz990487 <alik990487@gmail.com>
2024-03-09 17:35:03 +0500

    added Edit functionality

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.home, name='home'),
     path("room_page/<str:pk>/", views.room, name='room'), # pk for primary key,
-    path('create-room/', views.create_room, name='create-room')
+    path('create-room/', views.create_room, name='create-room'),
+    path('update-room/<str:pk>/', views.update_room, name='update-room'),
 ]
