20a528c38a40be4e33cd74c0a1021c11e16b2481
AliNiaz990487 <alik990487@gmail.com>
2024-03-09 17:48:47 +0500

    added delete functionality

--- base/urls.py
+++ base/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("room_page/<str:pk>/", views.room, name='room'), # pk for primary key,
     path('create-room/', views.create_room, name='create-room'),
     path('update-room/<str:pk>/', views.update_room, name='update-room'),
+    path('delete-room/<str:pk>/', views.delete_room, name='delete-room'),
 ]
